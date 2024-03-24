e62c5c02fa8b3573a17a0a4c3b9f80fed843e3d4
felipe <083felype@gmail.com>
2024-03-24 01:14:43 -0300

    feat: add initial Header component

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+  return (
+    <header className="m-auto w-full max-w-[900px] px-0 py-10">
+      <h1 className="m-0 p-0 text-3xl">Cadastro de Desenvolvedor</h1>
+      <p className="text-sm text-zinc-400">
+        Faça seu cadastro na lista e receba um emprego instantaneamente
+      </p>
+    </header>
+  )
+}
