968be531984a843352458ba4e3e11765f8f604ac
felipe <083felype@gmail.com>
2024-03-25 10:58:07 -0300

    chore: add formReducer

---
+++ src/reducers/form-reducer.ts
@@ -0,0 +1,37 @@
+export enum FormActions {
+  setCurrentStep,
+  setName,
+  setLevel,
+  setEmail,
+  setGithub,
+}
+
+export interface FormAction {
+  type: FormActions
+  payload: any
+}
+
+export interface FormState {
+  currentStep: number
+  name: string
+  level: 0 | 1
+  email: string
+  github: string
+}
+
+export function formReducer(state: FormState, action: FormAction) {
+  switch (action.type) {
+    case FormActions.setCurrentStep:
+      return { ...state, currentStep: action.payload }
+    case FormActions.setName:
+      return { ...state, name: action.payload }
+    case FormActions.setEmail:
+      return { ...state, email: action.payload }
+    case FormActions.setGithub:
+      return { ...state, github: action.payload }
+    case FormActions.setLevel:
+      return { ...state, level: action.payload }
+    default:
+      return state
+  }
+}
